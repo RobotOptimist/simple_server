afecccdb65439050fafdb4ee94b39bbd431e5f3d
RobotOptimist <james.macivor1@gmail.com>
2014-09-14 15:22:24 -0400

    browser exercise
    just experimenting with POST and GET, not to concerned with writing good
    code.

--- tiny_browser.rb
+++ tiny_browser.rb
@@ -1,15 +1,61 @@
 require 'socket'
- 
-host = 'localhost'     # The web server
-port = 3000                         # Default localhost port
-path = "./index.html"                 # The file we want 
+require 'uri'
+require 'json'
 
-# This is the HTTP request we send to fetch a file
-request = "GET #{path} HTTP/1.0\r\n\r\n"
 
-socket = TCPSocket.open(host,port)  # Connect to server
-socket.print(request)               # Send request
-response = socket.read              # Read complete response
-# Split response at first blank line into headers and body
-headers,body = response.split("\r\n\r\n", 2) 
-print body                          # And display it
+class Browser
+
+def initialize
+	@host = 'localhost'     # The web server
+	@port = 3000            # Default localhost port
+	@path = "./thanks.html" # The file we want 
+end
+
+
+def get_info
+	puts "What is your viking's name?"
+	name = gets.chomp
+	puts "What is you viking's email address?"
+	email = gets.chomp
+	{:viking => {:name => name, :email=> email}}.to_json
+end
+
+
+def get_request 
+	request = "GET #{@path} HTTP/1.0\r\n\r\n"
+end
+
+def post_request
+	viking = get_info
+	request = "POST #{@path} HTTP/1.0\r\n\r\n" +
+						"From: self" +
+						"User-Agent: HTTPTool/1.0" +
+						"Content-Type: text/json" +
+						"Content-Length: #{viking.size}"
+end
+
+def offer_choice
+	puts "Do you want to view the page or post information?"
+	choice = gets.chomp
+	case choice
+	when "view" then get_request
+	when "post" then post_request
+	else 
+		"I don't know what that means"
+		offer_choice
+	end
+end
+
+def connection(request)
+	socket = TCPSocket.open(@host,@port)  # Connect to server
+	socket.print(request)               # Send request
+	response = socket.read              # Read complete response
+	display(response)
+end
+
+def display(response)
+	headers,body = response.split("\r\n\r\n", 2) 
+	print body 
+end                         # And display it
+
+end
